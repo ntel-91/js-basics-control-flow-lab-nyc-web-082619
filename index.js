7a3ad16df6b8138f0e44e3fcf48a0cd1a765d874
Nick Telenson <ntelenson@gmail.com>
2019-10-05 11:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
